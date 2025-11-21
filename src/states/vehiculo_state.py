966fe67ede50a41e1fffc1131930c1692afeaa88
Francisco Ezequiel Andrade <403499@sistemas.frc.utn.edu.ar>
2025-11-21 03:58:27 -0300

    se agrego el patron state de vehiculo

--- src/states/vehiculo_state.py
+++ src/states/vehiculo_state.py
@@ -1,13 +1,58 @@
-from ..models.enums import EstadoVehiculo
+from __future__ import annotations
+from abc import ABC, abstractmethod
 from ..exceptions.domain_exceptions import BusinessException
 
 
-class VehiculoState:
+class Vehiculo:
+    _state: EstadoVehiculo = None
 
-    def alquilar(self, ctx):
-        raise BusinessException(
-            "Operación no permitida para el estado actual del vehículo")
+    def __init__(self, state: EstadoVehiculo) -> None:
+        self.transition_to(state)
 
-    def devolver(self, ctx):
-        raise BusinessException(
-            "Operación no permitida para el estado actual del vehículo")
+    def transition_to(self, state: EstadoVehiculo):
+        self._state = state
+        self._state.context = self
+
+    def alquilar(self):
+        return self._state.alquilar()
+
+    def devolver(self):
+        return self._state.devolver()
+
+
+class EstadoVehiculo(ABC):
+
+    @property
+    def context(self) -> Vehiculo:
+        return self._context
+
+    @context.setter
+    def context(self, context: Vehiculo) -> None:
+        self._context = context
+
+    @abstractmethod
+    def alquilar(self) -> str:
+        pass
+
+    @abstractmethod
+    def devolver(self) -> str:
+        pass
+
+
+class Disponible(EstadoVehiculo):
+    def alquilar(self) -> str:
+        self.context.transition_to(Alquilado())
+        return "El vehiculo fue alquilado"
+
+    def devolver(self) -> str:
+        raise BusinessException("El vehiculo ya está disponible")
+
+
+class Alquilado(EstadoVehiculo):
+    def alquilar(self) -> str:
+        raise BusinessException("El vehiculo ya está alquilado")
+
+    def devolver(self) -> str:
+        self.context.transition_to(Disponible())
+        return "El vehiculo fue devuelto"
+
