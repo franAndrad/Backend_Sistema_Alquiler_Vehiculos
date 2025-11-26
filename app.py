9617324ee316d4075b81b4883992292adc38e93b
Francisco Ezequiel Andrade <403499@sistemas.frc.utn.edu.ar>
2025-11-26 02:18:10 -0300

    se cambio el cors para usar PATCH

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def create_app():
         resources={r"/*": {"origins": ["http://localhost:3000"]}},
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
-        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     )
 
     app.json_provider_class = UTF8JSONProvider
